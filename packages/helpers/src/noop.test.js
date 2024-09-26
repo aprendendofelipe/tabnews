8c3b0aac244a078b1055da2fca9b52de45c035ee
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2024-09-25 23:04:28 -0300

    feat(helpers): add `No-OP` utility functions

---
+++ packages/helpers/src/noop.test.js
@@ -0,0 +1,32 @@
+import { identity, noop, returnNull } from '.'; // ajuste o caminho conforme necessário
+
+describe('helpers', () => {
+  describe('No-Op and Identity Functions', () => {
+    const inputs = [, 42, '42', 'hello', null, undefined, { key: 'value' }, [1, 2, 3]];
+    expect(inputs.length).toBe(8);
+
+    describe('identity', () => {
+      it('should return the same value that is passed in', () => {
+        inputs.forEach((input) => {
+          expect(identity(input)).toStrictEqual(input);
+        });
+      });
+    });
+
+    describe('noop', () => {
+      it('should not throw an error when called', () => {
+        inputs.forEach((input) => {
+          expect(noop(input)).toBeUndefined();
+        });
+      });
+    });
+
+    describe('returnNull', () => {
+      it('should return null', () => {
+        inputs.forEach((input) => {
+          expect(returnNull(input)).toBeNull();
+        });
+      });
+    });
+  });
+});
