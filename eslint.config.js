dc6e74358df0239d73d762bf00df0c09a28a8a6a
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2024-10-10 11:12:10 -0300

    chore(@tabnews/config): update to v0.2.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,1 @@
-import defaultConfig from '@tabnews/config/eslint';
-
-const config = [
-  ...defaultConfig,
-  {
-    ignores: ['**/.next/**', '**/__snapshots__/*', '**/dist/*'],
-  },
-];
-
-export default config;
+export { default } from '@tabnews/config/eslint';
