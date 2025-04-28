c88215aa7a60508699e42825fde572a2f281050b
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-04-28 11:56:09 -0300

    refactor(SCRegistry): remove unnecessary React import

--- packages/ui/src/SCRegistry/SCRegistry.jsx
+++ packages/ui/src/SCRegistry/SCRegistry.jsx
@@ -1,7 +1,7 @@
 'use client';
 // https://github.com/vercel/next.js/blob/7474f10c9616a504be5f79e731c6f6465e7dc2e2/examples/with-styled-components/lib/styled-components-registry.tsx
 import { useServerInsertedHTML } from 'next/navigation';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 export function StyledComponentsRegistry({ children }) {
