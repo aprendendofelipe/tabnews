ecf3ae5f8eaa001f965667e00825b03dac7ee696
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-05-21 19:28:17 -0300

    feat(helpers/url): add `isTrustedDomain` function and related tests

--- packages/helpers/src/index.js
+++ packages/helpers/src/index.js
@@ -10,7 +10,10 @@ export {
   getBaseUrl,
   getDomain,
   isExternalLink,
+  isTrustedDomain,
   replaceParams,
+  trustedDomains,
   tryParseUrl,
+  webserverDomain,
   webserverHostname,
 } from './url.js';
