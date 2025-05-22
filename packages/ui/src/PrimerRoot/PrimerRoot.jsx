82adaffce5f63fe5f687f55a5814fa492fb74360
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-05-22 13:51:47 -0300

    feat(PrimerRoot): add support for headChildren and additional HTML props

--- packages/ui/src/PrimerRoot/PrimerRoot.jsx
+++ packages/ui/src/PrimerRoot/PrimerRoot.jsx
@@ -1,23 +1,20 @@
 'use server';
+/* eslint-disable @next/next/no-head-element */
 import { cookies } from 'next/headers';
 
 import { AutoThemeProvider } from '../AutoThemeProvider/AutoThemeProvider.jsx';
 import { COLOR_MODE_COOKIE } from '../constants/public.js';
 import { StyledComponentsRegistry } from '../SCRegistry/SCRegistry.jsx';
 
-export async function PrimerRoot({ children, colorMode, defaultColorMode, lang, ...props }) {
+export async function PrimerRoot({ children, colorMode, defaultColorMode, headChildren, htmlProps, lang, ...props }) {
   const cookieStore = await cookies();
   const cachedColorMode = await cookieStore.get(COLOR_MODE_COOKIE);
   const ssrColorMode = (colorMode || cachedColorMode?.value || defaultColorMode) === 'dark' ? 'dark' : 'light';
 
   return (
-    <html
-      lang={lang}
-      suppressHydrationWarning
-      data-color-mode={ssrColorMode}
-      data-light-theme="light"
-      data-dark-theme="dark">
-      <body>
+    <html lang={lang} suppressHydrationWarning data-color-mode={ssrColorMode} {...htmlProps}>
+      <head>{headChildren}</head>
+      <body data-light-theme="light" data-dark-theme="dark">
         <StyledComponentsRegistry>
           <AutoThemeProvider defaultColorMode={ssrColorMode} noFlash={false} preventSSRMismatch {...props}>
             {children}
