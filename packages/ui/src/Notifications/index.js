ebdb422fca5cd9cf3a79cee41e841d38a2532509
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-07-02 08:45:21 -0300

    feat(Notifications): add NotificationMenu component with associated tests and configuration

--- packages/ui/src/Notifications/index.js
+++ packages/ui/src/Notifications/index.js
@@ -1,2 +1,3 @@
 export { NotificationList } from './NotificationList.jsx';
+export { NotificationMenu } from './NotificationMenu.jsx';
 export { NotificationsProvider, useNotifications } from './Provider.jsx';
--- packages/ui/src/Notifications/types.js
+++ packages/ui/src/Notifications/types.js
@@ -15,10 +15,21 @@
  */
 
 /**
+ * Generates a label for the bell icon based on the number of unread notifications.
+ *
+ * @callback GetBellLabel
+ * @param {number} count - The number of unread notifications.
+ * @returns {string} The label for the bell icon.
+ */
+
+/**
  * @typedef {object} NotificationLabels
  * @property {string} [empty] - Message for when there are no notifications.
  * @property {string} [loading] - Message for when notifications are loading.
  * @property {string} [notifications] - Title for the notifications menu.
+ * @property {string} [close] - Label for the close button.
+ * @property {string} [openActionsMenu] - Label for the button to open the actions menu.
+ * @property {GetBellLabel} [getBellLabel] - Generates aria-label for the bell icon.
  */
 
 /**
@@ -27,7 +38,7 @@
  */
 
 /**
- * @typedef {object} NotificationsContextType
+ * @typedef {object} NotificationCommonConfig
  * @property {NotificationAction[]} [actions] - Actions for each notification item.
  * @property {function(NotificationItem): JSX.Element | null} [getItemIcon] - Function to get the icon for a notification item.
  * @property {function(NotificationItem): boolean} [isItemRead] - Function to check if a notification item is read.
@@ -36,4 +47,16 @@
  * @property {NotificationItem[]} [notifications] - Array of notification objects.
  * @property {function(NotificationItem): void} [onItemSelect] - Callback function when the notification item is selected.
  * @property {NotificationSelectors} [selectors] - An object containing CSS selectors for elements.
+ */
+
+/**
+ * @typedef {object} NotificationsMenuConfig
+ * @property {function(NotificationItem[]): number} [getCount] - Function to get the count of notifications.
+ * @property {boolean} [isMenuOpen] - Whether the menu is currently open.
+ * @property {function(): void} [onCloseMenu] - Callback function when the menu is closed.
+ * @property {function(boolean): void} [setMenuOpen] - Function to update the menu's open state.
+ */
+
+/**
+ * @typedef {NotificationCommonConfig & NotificationsMenuConfig} NotificationsContextType
  */
