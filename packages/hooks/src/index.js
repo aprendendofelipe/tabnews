11af86abd8bbe78e49cf666a01fdaa292be3f14d
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-12-29 12:54:21 -0300

    feat(useMediaQuery): add new `useMediaQuery` hook

--- packages/hooks/src/index.js
+++ packages/hooks/src/index.js
@@ -1,3 +1,4 @@
 export * from './useConfig/index.js';
+export * from './useMediaQuery/index.js';
 export * from './useMergedState/index.js';
 export * from './useTreeCollapse/index.js';
---
+++ packages/hooks/src/useMediaQuery/useMediaQuery.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export function useMediaQuery(query) {
+  const [matches, setMatches] = useState(() => typeof window !== 'undefined' && !!window.matchMedia(query).matches);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    const listener = () => setMatches(media.matches);
+
+    listener();
+
+    media.addEventListener('change', listener);
+    return () => media.removeEventListener('change', listener);
+  }, [query]);
+
+  return matches;
+}
