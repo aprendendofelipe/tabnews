7f2e22296487c73ab28e8750520b786b3fa3d97b
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-12-30 10:38:05 -0300

    feat(example): add `Head` component for dynamic title and description management

--- examples/form/pages/registration.jsx
+++ examples/form/pages/registration.jsx
@@ -1,10 +1,12 @@
 import { DefaultLayout } from '../components/DefaultLayout.jsx';
+import { Head } from '../components/Head/Head.Pages.jsx';
 import { Registration } from '../components/Registration.jsx';
 import { registrationFields, store } from '../form-config.js';
 
 export default function RegistrationPage() {
   return (
     <DefaultLayout containerWidth="small">
+      <Head title="Pages Router · Custom Title" />
       <Registration fields={registrationFields} store={store} />
     </DefaultLayout>
   );
