b3669b8265238ff31c9fbcd97481e83936a8328d
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-12-30 10:37:05 -0300

    feat(example): add dynamic favicon based on color scheme via `useMediaQuery` hook

--- examples/form/app/layout.jsx
+++ examples/form/app/layout.jsx
@@ -2,11 +2,13 @@ import { PrimerRoot } from '@barso/ui';
 import '@barso/ui/css';
 
 import { DefaultLayout } from '../components/DefaultLayout.jsx';
+import { DefaultHead } from '../components/Head/Head.App';
 import { NotificationsProvider } from '../components/Notifications';
 
 export default function Layout({ children }) {
   return (
     <PrimerRoot defaultColorMode="light" lang="pt-BR">
+      <DefaultHead />
       <NotificationsProvider>
         <DefaultLayout containerWidth="medium">{children}</DefaultLayout>
       </NotificationsProvider>
